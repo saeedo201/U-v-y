483ac0a7fd09f5355835fb870062fa2e106f54cf
Saeed Ahmeed <44009760+saeedo201@users.noreply.github.com>
2021-04-03 22:47:50 +0200

    Create helpers.py

---
+++ helpers.py
@@ -0,0 +1,15 @@
+url_input = """
+MDTextField:
+    id: txt_input
+    input_type: "url"
+    hint_text: "Enter a url"
+    helper_text: "type some url to download it!"
+    helper_text_mode: "on_focus"
+    required: True
+    icon_right: "youtube"
+    icon_right_color: app.theme_cls.primary_color
+    pos_hint:{'center_x': 0.5, 'center_y': 0.6}
+    size_hint_x:None
+    width:670
+    
+"""
